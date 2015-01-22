98d8366705cae7e0a4147b6959cfb1582304e57f
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-01-22 13:22:50 +0100

    Bug 1120471: Added timerfd API for Android versions before Kitkat
    Android kernels before Kitkat support the timerfd API, but bionic
    doesn't. This commit adds the missing functions on Android API
    versions 14 to 18.

--- src/Android.mk
+++ src/Android.mk
@@ -1,9 +1,17 @@
 LOCAL_PATH:= $(call my-dir)
 
+local_source_files_14 := ports/timerfd.c
+local_source_files_15 := $(local_source_files_14)
+local_source_files_16 := $(local_source_files_15)
+local_source_files_17 := $(local_source_files_16)
+local_source_files_18 := $(local_source_files_17)
+local_source_files    := $(local_source_files_$(PLATFORM_SDK_VERSION))
+
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= fdstate.c \
-                  loop.c \
-                  task.c
+LOCAL_SRC_FILES := fdstate.c \
+                   loop.c \
+                   task.c \
+                   $(local_source_files)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 LOCAL_CFLAGS := -DANDROID_VERSION=$(PLATFORM_SDK_VERSION) -Wall -Werror
 LOCAL_SHARED_LIBRARIES := libcutils liblog
