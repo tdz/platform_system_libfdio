37b8a2011748a394e0592a6937644d138212c30b
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-08-20 15:09:03 +0200

    Bug 1196727: Fix timerfd syscalls on old ICS and JB
    The syscall numbers for the timerfd API are missing in ICS and
    old JB releases; although the kernel actually supports this feature.
    
    This patch defines the sycalls numbers on such old systems. The
    kernel API is extremly stable and these numbers should never change.

--- src/ports/timerfd.c
+++ src/ports/timerfd.c
@@ -14,14 +14,80 @@
  * limitations under the License.
  */
 
-#include <sys/syscall.h>
 #include "sys/timerfd.h"
+#include <sys/syscall.h>
 
 /* The C library's implementation of the timerfd API just moves
  * the arguments into the right places and triggers the system
  * call. We can do the same by using the syscall function. The
- * __NR_ constants are available on all platforms.
+ * __NR_ constants are available on all platforms, except some
+ * ancient releases of ICS and JB.
  */
+
+#if ANDROID_VERSION <= 16
+
+/* Detect target architecture */
+#if __GNUC__
+#if __arm__
+#define ARCH_ARM
+#elif __i386__
+#define ARCH_X86
+#elif __mips__
+#define ARCH_MIPS
+#else
+#error "Target architecture missing"
+#endif
+#endif
+
+#ifdef ARCH_ARM
+
+#ifndef __NR_timerfd_create
+#define __NR_timerfd_create (__NR_SYSCALL_BASE+350)
+#endif /* __NR_timerfd_create */
+
+#ifndef __NR_timerfd_settime
+#define __NR_timerfd_settime (__NR_SYSCALL_BASE+353)
+#endif /* __NR_timerfd_settime */
+
+#ifndef __NR_timerfd_gettime
+#define __NR_timerfd_gettime (__NR_SYSCALL_BASE+354)
+#endif /* __NR_timerfd_gettime */
+
+#elif ARCH_MIPS
+
+/* MIPS has get and set reversed */
+
+#ifndef __NR_timerfd_create
+#define __NR_timerfd_create (__NR_SYSCALL_BASE+284)
+#endif /* __NR_timerfd_create */
+
+#ifndef __NR_timerfd_gettime
+#define __NR_timerfd_gettime (__NR_SYSCALL_BASE+285)
+#endif /* __NR_timerfd_gettime */
+
+#ifndef __NR_timerfd_settime
+#define __NR_timerfd_settime (__NR_SYSCALL_BASE+286)
+#endif /* __NR_timerfd_settime */
+
+#elif ARCH_X86
+
+#ifndef __NR_timerfd_create
+#define __NR_timerfd_create (__NR_SYSCALL_BASE+322)
+#endif /* __NR_timerfd_create */
+
+#ifndef __NR_timerfd_settime
+#define __NR_timerfd_settime (__NR_SYSCALL_BASE+325)
+#endif /* __NR_timerfd_settime */
+
+#ifndef __NR_timerfd_gettime
+#define __NR_timerfd_gettime (__NR_SYSCALL_BASE+326)
+#endif /* __NR_timerfd_gettime */
+
+#else
+#error "Syscall numbers for timerfd missing"
+#endif
+
+#endif /* ANDROID_VERSION <= 16 */
 
 int
 timerfd_create(int clockid, int flags)
